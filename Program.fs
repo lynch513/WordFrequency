f46e0421679fda7381994da2a20d39926388c623
Sergey M Solomkin <lynch513@yandex.ru>
2020-11-26 22:09:04 +0300

    Fixed encoding in Galmet.txt. Added main staff in Program.fs

--- Program.fs
+++ Program.fs
@@ -1,11 +1,41 @@
-﻿open System
+﻿open System.IO
 
-// Define a function to construct a message to print
-let from whom =
-    sprintf "from %s" whom
+let readLines (filePath : string) = seq {
+    use sr = new StreamReader (filePath)
+    while not sr.EndOfStream do
+        yield sr.ReadLine ()
+}
+
+let wordSeq (xs: string seq) =
+    xs
+    |> Seq.collect (fun (x: string) -> x.Split([|','; ' '; ':'; '!'; '.'|])) 
+    |> Seq.map (fun (x: string) -> x.Trim ())
+
+let seq2FrequencyMap xs =
+    Seq.fold (fun (acc : Map<_, int>) i ->
+        if Map.containsKey i acc
+        then Map.add i ((Map.find i acc) + 1) acc
+        else Map.add i 1 acc)
+        Map.empty
+        xs
+
+let map2SorterList xs =
+    xs
+    |> Map.toList
+    |> List.sortWith (fun (k1, v1) (k2, v2) -> -compare v1 v2) 
+
+let takeMostFrequencyWordsFromFile filePath wordCount =
+        readLines filePath 
+        |> wordSeq
+        |> seq2FrequencyMap
+        |> map2SorterList
+        |> List.filter (fun (k, v) -> k.Length > 3)
+        |> Seq.take wordCount
+
 
 [<EntryPoint>]
 let main argv =
-    let message = from "F#" // Call the function
-    printfn "Hello world %s" message
+    printfn "10 most common words in Gamlet:"
+    for (word, freq) in takeMostFrequencyWordsFromFile @"./Gamlet.txt" 10 do
+        printfn "%s: %i" word freq
     0 // return an integer exit code
