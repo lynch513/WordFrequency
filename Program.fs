3869a915b147eaffb27f511d2266c90ec780e1b8
Sergey M Solomkin <lynch513@yandex.ru>
2020-11-27 15:25:57 +0300

    Fixed README.md. Refactored Program.fs

--- Program.fs
+++ Program.fs
@@ -1,24 +1,32 @@
 ﻿open System.IO
+open System
+
 
 module IO =
-    let readLines (filePath : string) = seq {
+    let readLines (filePath: string) = seq {
         use sr = new StreamReader (filePath)
         while not sr.EndOfStream do
             yield sr.ReadLine ()
     }
 
 module Words =
-    let splitString (x: string) =
+    let private splitString (x: string) =
         x.Split([|','; ' '; ':'; '!'; '.'|])
 
-    let trimString (x: string) =
+    let private trimString (x: string) =
         x.Trim ()
 
-    let wordSeq (xs: string seq) =
+    let wordsFromSeq (xs: string seq) =
         xs
         |> Seq.collect (splitString >> Array.map trimString)
 
-    let seq2FrequencyMap xs =
+    let wordsFromString (x: string) =
+        x
+        |> splitString
+        |> Array.map trimString
+        |> Seq.ofArray
+
+    let private seq2FrequencyMap xs =
         Seq.fold (fun (acc : Map<_, int>) i ->
             if Map.containsKey i acc
             then Map.add i ((Map.find i acc) + 1) acc
@@ -26,27 +34,55 @@ module Words =
             Map.empty
             xs
 
-    let map2SorterList xs =
+    let private map2SorterList xs =
         xs
         |> Map.toList
         |> List.sortWith (fun (k1, v1) (k2, v2) -> -compare v1 v2) 
 
-    let takeMostFrequencyFromSeq (xs: string seq) =
+    let countFrequencyAndSort (xs: string seq) =
+        xs
+        |> seq2FrequencyMap
+        |> map2SorterList
+        |> List.filter (fun (k, v) -> k.Length > 3)
+
+    let takeMostFrequencyWordsFromString (x: string) =
+            x 
+            |> wordsFromString
+            |> countFrequencyAndSort
+
+    let takeMostFrequencyWordsFromSeq (xs: string seq) =
             xs 
-            |> wordSeq
-            |> seq2FrequencyMap
-            |> map2SorterList
-            |> List.filter (fun (k, v) -> k.Length > 3)
+            |> wordsFromSeq
+            |> countFrequencyAndSort
+
+
+let application filePath wordCount =
+    let wordsFreq = 
+        filePath
+        |> IO.readLines
+        |> Words.takeMostFrequencyWordsFromSeq
+        |> Seq.take wordCount
+    printfn "%i most common words in %s:" wordCount filePath
+    for (word, freq) in wordsFreq do
+        printfn "%s: %i" word freq
 
 
 [<EntryPoint>]
 let main argv =
-    printfn "10 most common words in Gamlet:"
-    let mostFrequencyWordsList = 
-        @"./Gamlet.txt"
-        |> IO.readLines
-        |> Words.takeMostFrequencyFromSeq 
-        |> Seq.take 10
-    for (word, freq) in mostFrequencyWordsList do
-        printfn "%s: %i" word freq
+    let filePath = if argv.Length > 0 then Some(argv.[0]) else None 
+    let wordCount = if argv.Length > 1 then Some(argv.[1]) else None
+    let defaultWordCount = 10
+    
+    try
+        match filePath, wordCount with
+        | Some(filePath), None -> 
+            application filePath defaultWordCount
+        | Some(filePath), Some(wordCount) -> 
+            match Int32.TryParse wordCount with
+            | true, d -> 
+                application filePath d
+            | false, _ -> printfn "Argument wordCount must be integer"
+        | _, _ -> printfn "Usage: dotnet run -- <filePath> [wordCount]"
+    with 
+    | _ -> printfn "Argument <filePath>: file not found or input illegal path" 
     0 // return an integer exit code
---
+++ README.md
@@ -0,0 +1,40 @@
+# WordFrequency
+
+Util which allow you to find the most frequencies of words. This version for F#.
+
+## Usage
+
+* Download and install .NET Core [https://dotnet.microsoft.com/download](https://dotnet.microsoft.com/download) 
+* Clone this repo at some local directory. Go into it and type in console: 
+
+```console
+~$ dotnet restore 
+~$ dotnet run -- <filePath> [wordCount]
+```
+
+## Links
+
+[https://www.litres.ru/dmitriy-soshnikov/funkcionalnoe-programmirovanie-na-f-22075394/](https://www.litres.ru/dmitriy-soshnikov/funkcionalnoe-programmirovanie-na-f-22075394/)
+
+# WordFrequency
+
+Подсчет частоты встречающихся в тексте слов. Версия для F#.
+
+
+## Использование
+
+* Скачать и установить .NET Core [https://dotnet.microsoft.com/download](https://dotnet.microsoft.com/download) 
+* Клонировать этот репозитории в локальную директорию. Зайти в нее. И далее в консоли:
+
+```console
+~$ dotnet restore 
+~$ dotnet run -- <filePath> [wordCount]
+```
+
+## Список литературы
+
+[https://www.litres.ru/dmitriy-soshnikov/funkcionalnoe-programmirovanie-na-f-22075394/](https://www.litres.ru/dmitriy-soshnikov/funkcionalnoe-programmirovanie-na-f-22075394/)
+
+## Licence
+
+Copyright © 2019 FIXME
