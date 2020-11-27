7553ba5d964489c90d5b9353193ede897d4b5e18
Sergey M Solomkin <lynch513@yandex.ru>
2020-11-27 12:20:55 +0300

    Some refactoring

--- Program.fs
+++ Program.fs
@@ -1,48 +1,52 @@
 ﻿open System.IO
 
-let readLines (filePath : string) = seq {
-    use sr = new StreamReader (filePath)
-    while not sr.EndOfStream do
-        yield sr.ReadLine ()
-}
+module IO =
+    let readLines (filePath : string) = seq {
+        use sr = new StreamReader (filePath)
+        while not sr.EndOfStream do
+            yield sr.ReadLine ()
+    }
 
-let splitString (x: string) =
-    x.Split([|','; ' '; ':'; '!'; '.'|])
+module Words =
+    let splitString (x: string) =
+        x.Split([|','; ' '; ':'; '!'; '.'|])
 
-let trimString (x: string) =
-    x.Trim ()
+    let trimString (x: string) =
+        x.Trim ()
 
-let wordSeq (xs: string seq) =
-    xs
-    // |> Seq.collect (splitString >> Array.map trimString)
-    |> Seq.collect (fun (x: string) -> x.Split([|','; ' '; ':'; '!'; '.'|])) 
-    |> Seq.map (fun (x: string) -> x.Trim ())
-
-let seq2FrequencyMap xs =
-    Seq.fold (fun (acc : Map<_, int>) i ->
-        if Map.containsKey i acc
-        then Map.add i ((Map.find i acc) + 1) acc
-        else Map.add i 1 acc)
-        Map.empty
+    let wordSeq (xs: string seq) =
         xs
+        |> Seq.collect (splitString >> Array.map trimString)
 
-let map2SorterList xs =
-    xs
-    |> Map.toList
-    |> List.sortWith (fun (k1, v1) (k2, v2) -> -compare v1 v2) 
+    let seq2FrequencyMap xs =
+        Seq.fold (fun (acc : Map<_, int>) i ->
+            if Map.containsKey i acc
+            then Map.add i ((Map.find i acc) + 1) acc
+            else Map.add i 1 acc)
+            Map.empty
+            xs
 
-let takeMostFrequencyWordsFromFile filePath wordCount =
-        readLines filePath 
-        |> wordSeq
-        |> seq2FrequencyMap
-        |> map2SorterList
-        |> List.filter (fun (k, v) -> k.Length > 3)
-        |> Seq.take wordCount
+    let map2SorterList xs =
+        xs
+        |> Map.toList
+        |> List.sortWith (fun (k1, v1) (k2, v2) -> -compare v1 v2) 
+
+    let takeMostFrequencyFromSeq (xs: string seq) =
+            xs 
+            |> wordSeq
+            |> seq2FrequencyMap
+            |> map2SorterList
+            |> List.filter (fun (k, v) -> k.Length > 3)
 
 
 [<EntryPoint>]
 let main argv =
     printfn "10 most common words in Gamlet:"
-    for (word, freq) in takeMostFrequencyWordsFromFile @"./Gamlet.txt" 10 do
+    let mostFrequencyWordsList = 
+        @"./Gamlet.txt"
+        |> IO.readLines
+        |> Words.takeMostFrequencyFromSeq 
+        |> Seq.take 10
+    for (word, freq) in mostFrequencyWordsList do
         printfn "%s: %i" word freq
     0 // return an integer exit code
